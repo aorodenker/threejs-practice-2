e9033aea9877b542e88e8d01a31ca550e094a0a7
Andrew <andrew.orodenker@gmail.com>
2023-04-14 11:17:15 -0700

    14-go-live complete

---
+++ 13-3DText-14-Go-live/src/deploy-steps.js
@@ -0,0 +1,9 @@
+/*
+1) npm install vercel
+2) add deploy script:
+"deploy": "vercel --prod"
+3) npm run deploy
+4) follow steps in terminal:
+make sure Output Directory is dist
+5) link git repository to deployed project on Vercel
+*/
