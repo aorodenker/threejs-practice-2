eee17595c8b081983300cedac25198e247c3d8bb
Andrew <andrew.orodenker@gmail.com>
2023-05-16 16:00:11 -0700

    comment cleanup

--- 19-galaxy-generator/src/script.js
+++ 19-galaxy-generator/src/script.js
@@ -2,14 +2,10 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
 
-// Debug
 const gui = new dat.GUI({ width: 350 });
-// Canvas
 const canvas = document.querySelector('canvas.webgl');
-// Scene
 const scene = new THREE.Scene();
 
-// Galaxy
 const parameters = {};
 parameters.count = 100000;
 parameters.size = 0.01;
@@ -79,7 +75,6 @@ const generateGalaxy = () => {
 
 generateGalaxy();
 
-// GUI
 gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy);
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
@@ -90,7 +85,6 @@ gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange
 gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy);
 gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy);
 
-// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -107,25 +101,21 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.x = 3;
 camera.position.y = 3;
 camera.position.z = 3;
 scene.add(camera);
 
-// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
 const clock = new THREE.Clock();
 
 const tick = () => {
